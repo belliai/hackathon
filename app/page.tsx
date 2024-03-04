d5f3c9f044d4100500e8aebdcc5c673396a095cc
Arth Tyagi <arthtyagi7@gmail.com>
2024-03-04 18:36:54 +0530

    basic liveblocks setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Room } from "./Room";
+import { CollaborativeApp } from "./CollaborativeApp";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Room>
+        <CollaborativeApp />
+      </Room>
     </main>
   );
 }
