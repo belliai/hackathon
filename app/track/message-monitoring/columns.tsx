9ca83b3ce948c2d8044c7a429997bd160db61b4a
Hisyam <iam.ahmadhisyam@gmail.com>
2024-06-07 00:42:30 +0700

    Squashed commit of the following:
    commit 0b1994e8087e7889a7f2d6ca122be1088d08710e
    Author: Hisyam <iam.ahmadhisyam@gmail.com>
    Date:   Fri Jun 7 00:40:24 2024 +0700
    
        update sidebar
    
    commit e36d0d76c95c84e13f13144550005c652f6ebf05
    Author: Hisyam <iam.ahmadhisyam@gmail.com>
    Date:   Fri Jun 7 00:38:32 2024 +0700
    
        06.02 Track/Audit → Messaging → Monitor Messaging
    
    commit b23015d3ce704c2c67046249bebc81ccb99fc882
    Author: Hisyam <iam.ahmadhisyam@gmail.com>
    Date:   Thu Jun 6 23:50:45 2024 +0700
    
        06.01 Track/Audit →  Track AWB →  Track AWB updates

---
+++ app/track/message-monitoring/columns.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox"
+
+
+export const columns: ColumnDef<any>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={table.getIsAllPageRowsSelected()}
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "messageType",
+        header: "Message Type"
+    },
+    {
+        accessorKey: "message",
+        header: "Message"
+    },
+    {
+        accessorKey: "mailID",
+        header: "Mail ID"
+    },
+    {
+        accessorKey: "processed",
+        header: "Processed",
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.original.processed}
+                disabled
+                aria-label="Processed"
+            />
+        )
+    },
+    {
+        accessorKey: "status",
+        header: "Status"
+    },
+    {
+        accessorKey: "updatedBy",
+        header: "Updated By"
+    },
+    {
+        accessorKey: "updatedDate",
+        header: "Updated Date"
+    },
+    {
+        accessorKey: "error",
+        header: "Error"
+    }
+];
+    
+
+
+
+
