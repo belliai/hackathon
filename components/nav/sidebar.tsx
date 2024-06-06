dd157ace572a2b01b195858b543a91461cf02e3a
Rafiul <ahmadrafiulm@gmail.com>
2024-06-06 15:07:41 +0700

    feat: remove top spacing and change color of new order button

--- components/nav/sidebar.tsx
+++ components/nav/sidebar.tsx
@@ -3,10 +3,7 @@
 import { UserCircleIcon } from "@heroicons/react/24/outline";
 import { ChevronLeftIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
-import {
-  Boxes,
-  PlusSquare,
-} from "lucide-react";
+import { Boxes, PlusSquare } from "lucide-react";
 import { Button } from "../ui/button";
 import UserDropdown from "./UserDropdown";
 import NewOrderModal from "../dashboard/new-order-modal";
@@ -19,7 +16,7 @@ import { defaultNavigation } from "@/components/nav/data/defaultNavigation";
 const SIDEBAR_TYPE = {
   DEFAULT: 1,
   SETTING: 2,
-}
+};
 
 export default function SideBar() {
   const router = useRouter();
@@ -27,7 +24,7 @@ export default function SideBar() {
   const [sidebarType, setNavigationType] = useState(SIDEBAR_TYPE.DEFAULT);
 
   return (
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto px-5 pb-4 ring-border ring-1 no-scrollbar bg-black-background">
+    <div className="flex grow flex-col overflow-y-auto px-5 pb-4 ring-border ring-1 no-scrollbar bg-black-background">
       <div className="flex h-16 shrink-0 items-center justify-between">
         {sidebarType === SIDEBAR_TYPE.DEFAULT && (
           <UserDropdown doChangeNavigation={setNavigationType} />
@@ -37,7 +34,7 @@ export default function SideBar() {
             className="flex items-center gap-x-2 cursor-pointer"
             onClick={() => {
               setNavigationType(SIDEBAR_TYPE.DEFAULT);
-              router.push('/dashboard');
+              router.push("/dashboard");
             }}
           >
             <ChevronLeftIcon
@@ -57,9 +54,9 @@ export default function SideBar() {
                   <Button
                     variant="ghost"
                     onClick={() => setDialogOpen(true)}
-                    className="px-2 w-full py-5 justify-start mb-5 text-[13px] text-[#E2E3E5]"
+                    className="px-2 w-full h-8 justify-start mb-5 text-[13px] text-white bg-button-primary/60 hover:bg-button-primary/100 rounded-sm"
                   >
-                    <PlusSquare className="mr-2.5 h-[18px] w-[18px] text-[#949496]" />
+                    <PlusSquare className="mr-2.5 h-4 w-4" />
                     New Order
                   </Button>
                 </NewOrderModal>
@@ -77,7 +74,13 @@ export default function SideBar() {
               </li>
             )}
             <ul className="flex flex-col gap-1">
-              <SidebarMenu items={sidebarType === SIDEBAR_TYPE.SETTING ? settingNavigation : defaultNavigation} />
+              <SidebarMenu
+                items={
+                  sidebarType === SIDEBAR_TYPE.SETTING
+                    ? settingNavigation
+                    : defaultNavigation
+                }
+              />
             </ul>
             {sidebarType === SIDEBAR_TYPE.SETTING && (
               <>
@@ -95,7 +98,6 @@ export default function SideBar() {
                 </ul>
               </>
             )}
-            
           </ul>
         </ul>
       </nav>
