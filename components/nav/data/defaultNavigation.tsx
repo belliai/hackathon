b488237529d991b76fac9e2980d87f28f2950b0e
Tito Oktavian <tito.oktavian@yahoo.co.id>
2024-06-12 14:31:33 +0700

    04.12 Operations → Import → Import Inventory

--- components/nav/data/defaultNavigation.tsx
+++ components/nav/data/defaultNavigation.tsx
@@ -228,7 +228,7 @@ export const defaultNavigation: TSidebarItem[] = [
           { name: "Arrive", href: "/operation/import/arrive", current: false },
           { name: "Break ULD", href: "/operation/import/break-uld", current: false },
           { name: "Import Summary", href: "/operation/import/import-summary", current: false },
-          { name: "Import Inventory", href: "#", current: false },
+          { name: "Import Inventory", href: "/operation/import/import-inventory", current: false },
         ],
       },
       {
