58eb76c4812d1d24eac4ec64196433a3b4ca6d66
rui <172808h@mymail.nyp.edu.sg>
2024-07-02 22:59:11 +0800

    [Spot Rate List] Add header filters

--- app/sk/sales/spot-rate/list/page.tsx
+++ app/sk/sales/spot-rate/list/page.tsx
@@ -3,18 +3,100 @@
 import { Order, columns } from "@/components/dashboard/columns";
 import { getData } from "@/lib/data";
 import PageContainer from "@/components/layout/PageContainer";
-import RateLineNew from "@/components/sales/rate-line-new";
+import SpotRateDropDown from "@/components/sales/spot-rate/SpotRateDropDown";
+
+import { Input } from "@/components/ui/input"
+
+import { CalendarIcon } from "lucide-react";
+import { SpotRateDateIcon, SpotRateDatePicker } from "@/components/sales/spot-rate/SpotRateDatePicker";
+import { Button } from "@/components/ui/button";
 
 export default function SpotRateList() {
 
   return (
     <PageContainer className="py-8 gap-6">
+      <div>
         <h2 className="text-xl font-semibold mb-2">Spot Rate Listing</h2>
-        {/* <RateLineNew/> */}
 
-        {/* Header of filters */}
+        <div className="flex flex-col gap-4">
+          {/* Filters */}
+          <div className="flex flex-wrap gap-4">
+            {/* Origin and Agent Code */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Origin</label>
+                <SpotRateDropDown />
+                <SpotRateDropDown />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Agent Code</label>
+                <Input type="text" className="w-full" />
+              </div>
+            </div>
 
-        {/* Spot Rate Details */}
+            {/* Destination and Spot Rate ID */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Destination</label>
+                <SpotRateDropDown />
+                <SpotRateDropDown />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Spot Rate ID</label>
+                <Input type="text" className="w-full" />
+              </div>
+            </div>
+
+            {/* Flight# and AWBNumber */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Flight#</label>
+                <SpotRateDropDown className="w-full" />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">AWBNumber</label>
+                <Input type="text" className="w-14" defaultValue="807" />
+                <Input type="text" className="w-24" />
+              </div>
+            </div>
+
+            {/* Flight Date and Status */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Flight Date</label>
+                <Input type="text" className="w-24" />
+                <SpotRateDateIcon />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">Status</label>
+                <SpotRateDropDown />
+              </div>
+            </div>
+
+            {/* From Date and To Date */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">From Date</label>
+                <SpotRateDatePicker />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="whitespace-nowrap">To Date</label>
+                <SpotRateDatePicker />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Button> List</Button>
+        <Button>Clear</Button>
+        <Button>Export</Button>
+      </div>
+
+
+      {/* Spot Rate Details */}
     </PageContainer>
+
   );
 }
---
+++ components/sales/spot-rate/SpotRateDropDown.tsx
@@ -0,0 +1,35 @@
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { cn } from "@/lib/utils"
+
+const SpotRateDropDown = ({className}: {className?: string}) => {
+    return (
+        <>    
+        <Select>
+            <SelectTrigger className={cn("w-[180px]", className)}>
+                <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            
+            <SelectContent>
+                <SelectGroup>
+                <SelectItem value="apple">Option 1</SelectItem>
+                <SelectItem value="banana">Option 2</SelectItem>
+                <SelectItem value="blueberry">Option 3</SelectItem>
+                <SelectItem value="grapes">Option 4</SelectItem>
+                <SelectItem value="pineapple">Option 5</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+        </>
+    )
+}
+
+export default SpotRateDropDown
