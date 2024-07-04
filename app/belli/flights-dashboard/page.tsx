31cb08e52b32a3c186c9217e250671f7643a4037
Rafiul <ahmadrafiulm@gmail.com>
2024-07-04 15:54:04 +0700

    fix: flight dashboard table pagination

--- app/belli/flights-dashboard/page.tsx
+++ app/belli/flights-dashboard/page.tsx
@@ -1,14 +1,31 @@
 "use client"
 
+import { useCallback, useMemo, useState } from "react"
+import { PaginationState } from "@tanstack/react-table"
+
 import { useFlightList } from "@/lib/hooks/flight-master/flight-master"
 import { DataTable } from "@/components/data-table/data-table"
 import { columns } from "@/app/belli/flight-master/components/column"
 
 export default function FlightsDashboardPage() {
-  const { data: flights, isLoading } = useFlightList({
-    page: 1,
-    page_size: 10,
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
   })
+
+  const tableState = useCallback(async ({ pagination }: any) => {
+    setPagination(pagination)
+  }, [])
+
+  const paginationDetails = useMemo(
+    () => ({
+      page: pagination.pageIndex === 0 ? 1 : pagination.pageIndex + 1,
+      page_size: pagination.pageSize,
+    }),
+    [pagination]
+  )
+
+  const { data: flights, isLoading } = useFlightList(paginationDetails)
 
   const flightsData = flights?.data || []
 
@@ -29,8 +46,9 @@ export default function FlightsDashboardPage() {
         sector: false,
       }}
       data={flightsData}
-      pageCount={isLoading ? 1 : flights?.total_pages || 1}
+      pageCount={isLoading ? 1 : flights?.total_pages}
       manualPagination={true}
+      tableState={tableState}
       className="border-none [&_td]:px-3 [&_td]:py-1 [&_td]:text-muted-foreground [&_th]:px-3 [&_th]:py-2 [&_th]:text-foreground"
     />
   )
