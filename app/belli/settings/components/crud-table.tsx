acbcd6ef1dffb41d5b6e2f646b425011a75df356
abelramadhan <abelramadhan07@gmail.com>
2024-07-06 17:45:14 +0700

    feat(settings): adjust sidebar design

--- app/belli/settings/components/crud-table.tsx
+++ app/belli/settings/components/crud-table.tsx
@@ -21,6 +21,12 @@ import {
 } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {
@@ -49,6 +55,7 @@ import {
 } from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
+import InputSwitch, { InputSwitchProps } from "@/components/form/InputSwitch"
 
 type CrudTableProps<T extends FieldValues> = {
   title: string
@@ -56,81 +63,9 @@ type CrudTableProps<T extends FieldValues> = {
   id?: string
   isLoading?: boolean
   columns: ColumnDef<T>[]
-  form: {
-    name: Path<T>
-    label?: string
-    type: "text" | "select" | "hidden"
-    selectOptions?: { label: string; value: string }[]
-  }[]
+  form: InputSwitchProps<T>[]
   onSave: (data: T) => void
   onDelete: (data: T) => void
-}
-
-function InputSwitch<T extends FieldValues>(props: {
-  formField: CrudTableProps<T>["form"][0]
-}) {
-  const form = useFormContext<T>()
-  const { label, name, type, selectOptions } = props.formField
-  switch (type) {
-    case "select":
-      return (
-        <FormField
-          key={name}
-          control={form.control}
-          name={name}
-          render={({ field }) => (
-            <FormItem className="space-y-1">
-              <FormLabel className="text-xs font-semibold text-muted-foreground">
-                {label}
-              </FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {selectOptions?.map((option) => (
-                    <SelectItem key={option.value} value={option.value}>
-                      {option.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      )
-    case "hidden":
-      return (
-        <FormField
-          key={name}
-          control={form.control}
-          name={name}
-          render={({ field }) => <Input {...field} type="hidden" />}
-        />
-      )
-    default:
-      return (
-        <FormField
-          key={name}
-          control={form.control}
-          name={name}
-          render={({ field }) => (
-            <FormItem className="space-y-1">
-              <FormLabel className="text-xs font-semibold text-muted-foreground">
-                {label}
-              </FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      )
-  }
 }
 
 const FormDialog = <T extends FieldValues>(
@@ -167,7 +102,7 @@ const FormDialog = <T extends FieldValues>(
             </DialogHeader>
             <div className="space-y-1">
               {props.form.map((formField) => (
-                <InputSwitch key={formField.name} formField={formField} />
+                <InputSwitch<T> key={formField.name} {...formField} />
               ))}
             </div>
             <DialogFooter>
@@ -190,6 +125,71 @@ const FormDialog = <T extends FieldValues>(
         </Form>
       </DialogContent>
     </Dialog>
+  )
+}
+
+const FormDropdown = <T extends FieldValues>(
+  props: PropsWithChildren & {
+    form: CrudTableProps<T>["form"]
+    onSave: CrudTableProps<T>["onSave"]
+    data?: DefaultValues<T>
+  }
+) => {
+  const [value, setValue] = useState("")
+  const form = useForm<T>({
+    defaultValues: props.data,
+  })
+
+  const onSubmit = (data: T) => {
+    props.onSave(data)
+    setValue("")
+    form.reset()
+  }
+
+  return (
+    <Accordion value={value} onValueChange={setValue} collapsible type="single">
+      <AccordionItem value="item" className="space-y-4 border-b-0">
+        <AccordionTrigger
+          className="flex flex-row justify-end p-0 hover:no-underline"
+          customarrow={<></>}
+        >
+          <Button variant={"button-primary"}>Add new</Button>
+        </AccordionTrigger>
+        <AccordionContent className="border-none p-0">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+              <Card className="flex flex-row items-center justify-start gap-3 rounded-md p-4 shadow-md">
+                {props.form.map((formField) => (
+                  <InputSwitch
+                    key={formField.name}
+                    {...formField}
+                    label={undefined}
+                    placeholder={formField.label}
+                  />
+                ))}
+                <div className="flex gap-3">
+                  <Button
+                    type="button"
+                    className="rounded-sm"
+                    variant="secondary"
+                    onClick={() => setValue("")}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    className="rounded-sm"
+                    variant={"button-primary"}
+                    type="submit"
+                  >
+                    Save
+                  </Button>
+                </div>
+              </Card>
+            </form>
+          </Form>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
 
@@ -243,7 +243,8 @@ export default function CrudTable<T extends FieldValues>(
   })
 
   return (
-    <section id={props.id}>
+    <section id={props.id} className="space-y-4">
+      <FormDropdown form={props.form} onSave={props.onSave} />
       <Card className="overflow-clip rounded-md">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 bg-card px-4 py-2">
           <CardTitle className="text-lg font-bold">{title}</CardTitle>
