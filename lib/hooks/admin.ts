db94dcf9af92673070cb19ea005edffcecf1861e
Rafiul <ahmadrafiulm@gmail.com>
2024-07-13 19:56:09 +0700

    feat: hook to check admin role

---
+++ lib/hooks/admin.ts
@@ -0,0 +1,22 @@
+import { useOrganizationList } from "@clerk/nextjs"
+
+function useAdmin() {
+    const { userMemberships } = useOrganizationList({
+        userMemberships: {
+            infinite: true,
+        },
+    })
+
+
+    const isAdmin = userMemberships.data?.some(
+        (data) => data.organization.slug === "admin"
+    )
+
+
+    return {
+        isAdmin,
+        userMemberships
+    }
+}
+
+export { useAdmin }
